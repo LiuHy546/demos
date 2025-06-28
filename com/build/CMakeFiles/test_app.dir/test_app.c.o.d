f531876d66a456cbae2d4b37177dca512fcf01ca
Liuhy <142076871+LiuHy546@users.noreply.github.com>
2025-06-28 12:22:13 +0800

    Delete com directory

--- com/build/CMakeFiles/test_app.dir/test_app.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_app.dir/test_app.c.o: \
- /mnt/c/Users/LIUHY/Desktop/test/test_app.c /usr/include/stdc-predef.h \
- /mnt/c/Users/LIUHY/Desktop/test/main.h
--- com/build1/CMakeFiles/test_app.dir/special.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_app.dir/special.c.o: \
- /mnt/c/Users/LIUHY/Desktop/test/special.c /usr/include/stdc-predef.h \
- /mnt/c/Users/LIUHY/Desktop/test/main.h
